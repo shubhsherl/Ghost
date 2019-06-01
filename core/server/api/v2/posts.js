45ebe63a6b9c50874afbb5c5cb80dee95aa6bee8
shubhsherl <shubham.singh094@yahoo.com>
2019-06-01 14:36:53 +0530

    add redirect from rc

--- core/server/api/v2/posts.js
+++ core/server/api/v2/posts.js
@@ -1,6 +1,7 @@
 const models = require('../../models');
 const common = require('../../lib/common');
 const urlService = require('../../services/url');
+const rcUtils = require('./utils/rc-utils');
 const allowedIncludes = ['tags', 'authors', 'authors.roles'];
 const unsafeAttrs = ['status', 'authors'];
 
@@ -97,6 +98,49 @@ module.exports = {
             unsafeAttrs: unsafeAttrs
         },
         query(frame) {
+            const toAnnounce = frame.data.posts[0].to_announce || false;
+            if (toAnnounce) {
+                const roomName = frame.data.posts[0].room_name;
+                if (!roomName) {
+                    return Promise.reject(new common.errors.NotFoundError({
+                        message: 'Please provide a roomName to announce.'
+                    }));
+                    // throw new common.errors.NotFoundError({
+                    //     message: 'Please provide a roomName to annoucne.'
+                    // });
+                } else {
+                    let header = {
+                        'X-Auth-Token': 'Snhsgh5Q_q6y-ZlhGaN9AIbzN8iGCZEzfLAXNU9Y29G',
+                        'X-User-Id': 'AZG7dyTXMJoPhJHE7'
+                    };
+                    // validate if the room is accessible by user.
+                    return rcUtils.validateRoom('https://open.rocket.chat/api/v1/rooms.info', header, roomName)
+                        .then((r, err) => {
+                            if (err) {
+                                errors.push(err);
+                            } else if (!r || !r.exist) {
+                                return Promise.reject(new common.errors.NotFoundError({
+                                    message: 'Room doesnot exist. Make sure you have access to the room.'
+                                }));
+                                // throw new common.errors.NotFoundError({
+                                //     message: 'Room doesnot exist. Make sure you have access to the room.'
+                                // });
+                            } else if (r.name === roomName) {
+                                frame.data.posts[0].room_id = r.rid;
+                            }
+                            console.log(frame.data.posts[0])
+                            return models.Post.add(frame.data.posts[0], frame.options)
+                                .then((model) => {
+                                    if (model.get('status') !== 'published') {
+                                        this.headers.cacheInvalidate = false;
+                                    } else {
+                                        this.headers.cacheInvalidate = true;
+                                    }
+                                    return model;
+                                });
+                        });
+                }
+            }
             return models.Post.add(frame.data.posts[0], frame.options)
                 .then((model) => {
                     if (model.get('status') !== 'published') {
@@ -134,6 +178,58 @@ module.exports = {
             unsafeAttrs: unsafeAttrs
         },
         query(frame) {
+            const toAnnounce = frame.data.posts[0].to_announce || false;
+            const status = frame.data.posts[0].status;
+            if (toAnnounce && status === 'published') {
+                const roomName = frame.data.posts[0].room_name;
+                if (!roomName) {
+                    return Promise.reject(new common.errors.NotFoundError({
+                        message: 'Please provide a roomName to announce.'
+                    }));
+                } else {
+                    let header = {
+                        'X-Auth-Token': 'Snhsgh5Q_q6y-ZlhGaN9AIbzN8iGCZEzfLAXNU9Y29G',
+                        'X-User-Id': 'AZG7dyTXMJoPhJHE7'
+                    };
+                    // validate if the room is accessible by user.
+                    return rcUtils.validateRoom('https://open.rocket.chat/api/v1/rooms.info', header, roomName)
+                        .then((r, err) => {
+                            if (err) {
+                                errors.push(err);
+                            } else if (!r || !r.exist) {
+                                return Promise.reject(new common.errors.NotFoundError({
+                                    message: 'Room doesnot exist. Make sure you have access to the room.'
+                                }));
+                            } else if (r.name === roomName) {
+                                frame.data.posts[0].room_id = r.rid;
+                            }
+                            console.log(frame.data.posts[0])
+                            return models.Post.edit(frame.data.posts[0], frame.options)
+                                .then((model) => {
+                                    if (
+                                        model.get('status') === 'published' && model.wasChanged() ||
+                                        model.get('status') === 'draft' && model.previous('status') === 'published'
+                                    ) {
+                                        this.headers.cacheInvalidate = true;
+                                    } else if (
+                                        model.get('status') === 'draft' && model.previous('status') !== 'published' ||
+                                        model.get('status') === 'scheduled' && model.wasChanged()
+                                    ) {
+                                        this.headers.cacheInvalidate = {
+                                            value: urlService.utils.urlFor({
+                                                relativeUrl: urlService.utils.urlJoin('/p', model.get('uuid'), '/')
+                                            })
+                                        };
+                                    } else {
+                                        this.headers.cacheInvalidate = false;
+                                    }
+
+                                    return model;
+                                });
+                        });
+                }
+            }
+            console.log(frame.data.posts[0]);
             return models.Post.edit(frame.data.posts[0], frame.options)
                 .then((model) => {
                     if (
